2c773d2c9da6a0aa52c8ca372efd3e1cab90a2a1
Marvin <marvin_sanchez@live.com>
2017-12-07 18:04:38 -0500

    changes on files

--- example/test.py
+++ example/test.py
@@ -1,16 +1,16 @@
 import os
 import unittest
 
-from example.DestinyFileIO import getFilePathName
-from example.DestinyFileIO import getFileSize
+from DestinyFileIO import get_file_path_name
+from DestinyFileIO import get_file_size
 
 class MyTest(unittest.TestCase):
     def test_file_name(self):
-        self.assertEqual(getFilePathName("./example/mydata.txt"),
+        self.assertEqual(get_file_path_name("./example/mydata.txt"),
                          os.path.basename("./example/mydata.txt"))
 
     def test_file_size(self):
-        self.assertEqual(getFileSize("./example/mydata.txt"), 44)
+        self.assertEqual(get_file_size("./example/mydata.txt"), 44)
 
 if __name__ == '__main__':
     unittest.main()

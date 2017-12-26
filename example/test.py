5040e149dc3084b978ff5674d457cf2d7ab18125
Marvin <marvin_sanchez@live.com>
2017-12-25 21:42:49 -0500

    changes to file path on test.py
    changes to file path

--- example/test.py
+++ example/test.py
@@ -6,11 +6,11 @@ from DestinyFileIO import get_file_size
 
 class MyTest(unittest.TestCase):
     def test_file_name(self):
-        self.assertEqual(get_file_path_name("./example/mydata.txt"),
-                         os.path.basename("./example/mydata.txt"))
+        self.assertEqual(get_file_path_name("mydata.txt"),
+                         os.path.basename("mydata.txt"))
 
     def test_file_size(self):
-        self.assertEqual(get_file_size("./example/mydata.txt"), 44)
+        self.assertEqual(get_file_size("mydata.txt"), 44)
 
 if __name__ == '__main__':
     unittest.main()
